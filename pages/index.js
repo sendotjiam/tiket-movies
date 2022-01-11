ccce6fe46d1af79b9f9a8ec298b7406ef3550d39
Sendo Tjiamis <54806417+sendotjiam@users.noreply.github.com>
2022-01-11 10:13:00 +0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>BNCC Movies</title>
+        <meta name="description" content="BNCC Movies" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>Landing</main>
+    </>
+  )
+}
