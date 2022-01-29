a73ded93e80eb35f957f59512eecf02c2b61bd67
Sendo Tjiamis <sendotjiamis9898@gmail.com>
2022-01-29 14:58:54 +0700

    Added auth login UI

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,21 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>BNCC Movies</title>
-        <meta name="description" content="BNCC Movies" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>BNCC Movies</title>
+				<meta name='description' content='BNCC Movies' />
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
 
-      <main>Landing</main>
-    </>
-  )
+      <main>
+        <div>TEST</div>
+        <Link href="/login">
+          <a>LOGIN</a>
+        </Link>
+      </main>
+		</>
+	);
 }
---
+++ pages/login.js
@@ -0,0 +1,65 @@
+import {
+	Box,
+	Center,
+	Stack,
+	VStack,
+	FormControl,
+	FormLabel,
+	Input,
+	Button,
+	Text,
+} from '@chakra-ui/react';
+import { ChevronLeftIcon } from '@chakra-ui/icons';
+
+const Login = () => {
+	return (
+		<Stack
+			className={'stack'}
+			direction={['column', 'row']}
+			height={'100vh'}
+			width={'100vw'}
+		>
+			<Box flex='1' className='box' bg={'#2B6CB0'}>
+				<Center color={'white'} height={'100%'}>
+					<Button
+						colorScheme='white'
+						variant='link'
+						pos={'absolute'}
+						top={'50'}
+						left={'50'}
+					>
+						<ChevronLeftIcon w={'8'} h={'8'} />
+						<Text display={{ base: 'none', sm: 'block' }}>
+							Kembali ke Beranda
+						</Text>
+					</Button>
+					<VStack align={{ base: 'center', sm: 'start' }}>
+						<Text fontSize={'24px'}>Login ke akun</Text>
+						<Text fontSize={'24px'} fontWeight={'bold'}>
+							BNCC X Tiket Movies
+						</Text>
+					</VStack>
+				</Center>
+			</Box>
+			<Box flex='1' className='box' bg={'white'}>
+				<Center height={'100%'}>
+					<VStack width={'320px'}>
+						<FormControl>
+							<FormLabel htmlFor='email'>Email address</FormLabel>
+							<Input id='email' type='email' />
+						</FormControl>
+						<FormControl>
+							<FormLabel htmlFor='password'>Password</FormLabel>
+							<Input id='password' type='password' />
+						</FormControl>
+						<Button colorScheme='blue' width='100%'>
+							Login
+						</Button>
+					</VStack>
+				</Center>
+			</Box>
+		</Stack>
+	);
+};
+
+export default Login;
