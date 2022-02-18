0f915a21702062fcf4fb4b0c2e4070711e5c553d
Awesa <awesaatyanta.p@gmail.com>
2022-02-18 23:35:13 +0700

    UI and cast done

--- pages/detail/[movie_id].js
+++ pages/detail/[movie_id].js
@@ -1,11 +1,11 @@
 import {
     Box,
     Container,
-    Grid,
-    GridItem,
     Image,
     Text,
     Button,
+    Flex,
+    Spacer,
 } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
@@ -18,6 +18,7 @@ const detail = () => {
     const router = useRouter();
     const { movie_id } = router.query;
     const [movieData, setMovieData] = useState([]);
+    const [castData, setCastData] = useState([]);
     const [movieIsFavorite, setMovieIsFavorite] = useState([]);
 
     useEffect(() => {
@@ -27,6 +28,13 @@ const detail = () => {
                 console.log(res.data);
                 setMovieData(res.data);
                 checkMovieFavorite(res.data.id);
+            })
+            .catch((err) => console.log(err));
+        axios
+            .get(`${baseUrl}/movie/${movie_id}/credits?api_key=${apiKey}`)
+            .then((res) => {
+                console.log(res.data.cast);
+                setCastData(res.data.cast);
             })
             .catch((err) => console.log(err));
     }, [router.isReady]);
@@ -79,35 +87,96 @@ const detail = () => {
     };
 
     return (
-        <Container>
-            <Grid gridTemplateColumns={"2fr 3fr"} height={"100vh"}>
-                <GridItem bg="tomato">
-                    <Image
-                        borderRadius={5}
-                        src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movieData.poster_path}`}
-                        // Ini load nya lama banget
-                    />
-                </GridItem>
-                <GridItem bg="papayawhip">
-                    <Grid></Grid>
-                    <Text fontSize="48px" color="gray.700" fontWeight="600">
-                        {movieData.original_title}
-                    </Text>
-                    <Text>{getYear(movieData.release_date)}</Text>
-                    <Text>{movieData.tagline}</Text>
-                    <Text>{movieData.overview}</Text>
-                    <Button
-                        colorScheme="blue"
-                        width="100%"
-                        onClick={toggleFavorite}
-                    >
-                        {movieIsFavorite
-                            ? "Remove from favorite"
-                            : "Add to Favorite"}
-                    </Button>
-                </GridItem>
-            </Grid>
-        </Container>
+        <Box mt="64px">
+            <Container maxW="container.lg">
+                <Flex justifyContent="space-between">
+                    <Box>
+                        <Image
+                            borderRadius={5}
+                            objectFit="cover"
+                            maxWidth="300px"
+                            src={`https://www.themoviedb.org/t/p/w600_and_h900_bestv2${movieData.poster_path}`}
+                            // Ini load nya lama banget
+                        />
+                    </Box>
+                    <Box ml={"80px"}>
+                        <Flex mt="10px" alignItems="baseline">
+                            <Flex>
+                                <Text
+                                    fontSize="48px"
+                                    color="gray.700"
+                                    fontWeight="700"
+                                >
+                                    {movieData.original_title}
+                                </Text>
+                            </Flex>
+                            <Flex ml={"19px"}>
+                                <Text fontSize="24px">
+                                    {getYear(movieData.release_date)}
+                                </Text>
+                            </Flex>
+                            <Spacer />
+                            <Flex>
+                                {movieIsFavorite ? (
+                                    <Button
+                                        colorScheme="white"
+                                        variant="outline"
+                                        width="100%"
+                                        onClick={toggleFavorite}
+                                    >
+                                        Dalam list favoritmu
+                                    </Button>
+                                ) : (
+                                    <Button
+                                        colorScheme="blue"
+                                        width="100%"
+                                        onClick={toggleFavorite}
+                                    >
+                                        Favoritkan
+                                    </Button>
+                                )}
+                            </Flex>
+                        </Flex>
+                        <Box mt={"36px"}>
+                            <Text>{movieData.tagline}</Text>
+                        </Box>
+                        <Box mt={"36px"}>
+                            <Text>{movieData.overview}</Text>
+                        </Box>
+                    </Box>
+                </Flex>
+                <Box mt={"90px"}>
+                    <Box>
+                        <Text fontSize="30px" color="gray.700" fontWeight="700">
+                            Cast
+                        </Text>
+                    </Box>
+                    <Flex justifyContent="space-between">
+                        {castData.slice(0, 6).map((cast) => {
+                            return (
+                                <Container>
+                                    <Box
+                                        maxW="sm"
+                                        height="280px"
+                                        borderWidth="1px"
+                                        borderRadius="lg"
+                                        overflow="hidden"
+                                    >
+                                        <Image
+                                            src={`https://www.themoviedb.org/t/p/w138_and_h175_face${cast.profile_path}`}
+                                        />
+                                        <Text fontWeight="700">
+                                            {cast.name}
+                                        </Text>
+                                        <Text>{cast.character}</Text>
+                                    </Box>
+                                </Container>
+                            );
+                        })}
+                    </Flex>
+                </Box>
+            </Container>
+        </Box>
     );
 };
 
