5d759ff77eca0455744aed23b9e22ab6c8807e7f
Awesa <awesaatyanta.p@gmail.com>
2022-02-20 22:40:02 +0700

    Added logout functionality

--- pages/index.js
+++ pages/index.js
@@ -1,149 +1,140 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { ChakraProvider } from '@chakra-ui/react'
-import Movie from './Movie';
-import Login from './login';
-import React, {useEffect, useState} from 'react';
+import Head from "next/head";
+import Link from "next/link";
+import Movie from "./Movie";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import {
-	Box,
-	Text,
-	SimpleGrid,
-	Image,
-	Center,
-	FormControl,
-	Input,
-	Flex,
-	Button,
-	LinkBox,
-	LinkOverlay
-} from '@chakra-ui/react';
+    Box,
+    Text,
+    SimpleGrid,
+    Center,
+    FormControl,
+    Input,
+    Flex,
+    Button,
+} from "@chakra-ui/react";
 
 export default function Home() {
-	const BASE_URL = 'https://api.themoviedb.org/3';
-	const API_KEY = '393f424e1635f97da37ea1519f7d1d87';
-	const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=393f424e1635f97da37ea1519f7d1d87';
+    const baseURL = "https://api.themoviedb.org/3";
+    const apiKey = "f2f499786a0550c8e14677f17079dee1";
 
-	const [movies, setMovies] = useState([]);
+    const [sessionId, setSessionId] = useState([]);
+    const [movies, setMovies] = useState([]);
 
-	useEffect(() => {
-		fetch(API_URL)
-		.then(res => res.json())
-		.then(data => {
-			setMovies(data.results);
-			console.log(data.results);
-		});
-	}, []);
+    useEffect(() => {
+        axios
+            .get(
+                `${baseURL}/discover/movie?sort_by=popularity.desc&api_key=${apiKey}`
+            )
+            .then((res) => {
+                setMovies(res.data.results);
+                setSessionId(getSessionId());
+                console.log(res.data.results);
+            });
+    }, [sessionId]);
 
-	return (
-		<>
-			<Head>
-				<title>BNCC Movies</title>
-				<meta name='description' content='BNCC Movies' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
+    const getSessionId = () => {
+        return window.localStorage.getItem("session_id");
+    };
 
-      <main>
-        <Box d="flex" alignItems="center" justifyContent="space-between" pl='20' pr='20' pt='5' pb='5'>
-			<Box>
-				<Text fontSize={'20px'} fontWeight={'bold'} color={'blue.600'}>
-					BNCC X Tiket Movies
-				</Text>
-			</Box>
-			<Box>
-				<Link href="/login">
-				<a>Login</a></Link>
-			</Box>
-		</Box>
-		
-		<Box pl='20' pr='20' pt='10' pb='10' bg={'blue.600'}>
-			<Text fontSize={'30px'} fontWeight={'bold'} color='white'>
-				Populer Sekarang
-			</Text>
-			<Box marginBottom='5'></Box>
+    const deleteSessionId = () => {
+        window.localStorage.removeItem("session_id");
+        setSessionId(null);
+    };
 
-			
-			<SimpleGrid minChildWidth={{base: '100%', md: '15%', lg: '15%' , xl: '15%'}} spacing='12px'>
+    const logout = () => {
+        console.log("Logging Out!");
+        axios
+            .delete(`${baseURL}/authentication/session?api_key=${apiKey}`, {
+                data: {
+                    session_id: sessionId,
+                },
+            })
+            .then((res) => {
+                console.log(res);
+                deleteSessionId();
+            })
+            .catch((err) => console.log(err));
+    };
 
-				{movies.length > 0 && movies.slice(0, 5).map(movie => <Movie key={movie.id} {...movie} />)}
+    return (
+        <>
+            <Head>
+                <title>BNCC Movies</title>
+                <meta name="description" content="BNCC Movies" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-				{/* <LinkBox>
-					<LinkOverlay href = '#'>
-						<Box>
-							<Center>
-								<Image src = 'https://bit.ly/dan-abramov' alt = 'kontol'></Image>
-							</Center>
-							<Center fontSize={'15px'} fontWeight={'bold'} color='white' mt='10px'>
-								Film2
-							</Center>
-						</Box>
-					</LinkOverlay>
-				</LinkBox>
+            <main>
+                <Box
+                    d="flex"
+                    alignItems="center"
+                    justifyContent="space-between"
+                    pl="20"
+                    pr="20"
+                    pt="5"
+                    pb="5"
+                >
+                    <Box>
+                        <Text
+                            fontSize={"20px"}
+                            fontWeight={"bold"}
+                            color={"blue.600"}
+                        >
+                            BNCC X Tiket Movies
+                        </Text>
+                    </Box>
+                    <Box>
+                        {!sessionId ? (
+                            <Link href="/login">
+                                <a>Login</a>
+                            </Link>
+                        ) : (
+                            <a onClick={logout}>Logout</a>
+                        )}
+                    </Box>
+                </Box>
 
-				<LinkBox>
-					<LinkOverlay href = '#'>
-						<Box>
-							<Center>
-								<Image src = 'https://bit.ly/dan-abramov' alt = 'kontol'></Image>
-							</Center>
-							<Center fontSize={'15px'} fontWeight={'bold'} color='white' mt='10px'>
-								Film3
-							</Center>
-						</Box>
-					</LinkOverlay>
-				</LinkBox>
+                <Box pl="20" pr="20" pt="10" pb="10" bg={"blue.600"}>
+                    <Text fontSize={"30px"} fontWeight={"bold"} color="white">
+                        Populer Sekarang
+                    </Text>
+                    <Box marginBottom="5"></Box>
 
-				<LinkBox>
-					<LinkOverlay href = '#'>
-						<Box>
-							<Center>
-								<Image src = 'https://bit.ly/dan-abramov' alt = 'kontol'></Image>
-							</Center>
-							<Center fontSize={'15px'} fontWeight={'bold'} color='white' mt='10px'>
-								Film4
-							</Center>
-						</Box>
-					</LinkOverlay>
-				</LinkBox> */}
+                    <SimpleGrid
+                        minChildWidth={{
+                            base: "100%",
+                            md: "15%",
+                            lg: "15%",
+                            xl: "15%",
+                        }}
+                        spacing="12px"
+                    >
+                        {movies.length > 0 &&
+                            movies
+                                .slice(0, 5)
+                                .map((movie) => (
+                                    <Movie key={movie.id} {...movie} />
+                                ))}
+                    </SimpleGrid>
+                </Box>
 
-				{/* <LinkBox>
-					<LinkOverlay href = '#'>
-						<Box>
-							<Center>
-								<Image src = 'https://bit.ly/dan-abramov' alt = 'kontol'></Image>
-							</Center>
-							<Center fontSize={'15px'} fontWeight={'bold'} color='white' mt='10px'>
-								Film5
-							</Center>
-						</Box>
-					</LinkOverlay>
-				</LinkBox> */}
-				
-			</SimpleGrid>
-			
-		</Box>
-
-		<Center mt='30px'>
-			<Box>
-				<Center>
-					<Text fontSize={'20px'}>
-						Cari Film
-					</Text>
-				</Center>
-				<Flex>
-					<FormControl>
-						<Input
-							id='search'
-							type='text'
-						/>
-					</FormControl>
-					<Button colorScheme='blue' ml='5px'>
-						Cari
-					</Button>
-				</Flex>
-			</Box>
-		</Center>
-        
-      </main>
-		</>
-	);
+                <Center mt="30px">
+                    <Box>
+                        <Center>
+                            <Text fontSize={"20px"}>Cari Film</Text>
+                        </Center>
+                        <Flex>
+                            <FormControl>
+                                <Input id="search" type="text" />
+                            </FormControl>
+                            <Button colorScheme="blue" ml="5px">
+                                Cari
+                            </Button>
+                        </Flex>
+                    </Box>
+                </Center>
+            </main>
+        </>
+    );
 }
